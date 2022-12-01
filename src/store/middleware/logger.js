9c69d058360db803604a2b757c0ddba0528926b5
Nielsen <fredenielsen@deloitte.dk>
2022-12-01 13:07:56 +0100

    S15 e167 Redux-Persist so it remebers what you have put in your basket like localstorage

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,15 @@
+// we can use redux-logger own middleware outcommed in store
+export const loggerMiddleware = (store)  => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+
+
+    console.log('next state: ', store.getState());
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import logger from 'redux-logger';
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { loggerMiddleware } from "./middleware/logger";
 
 // Middle wears our kind of like little library helpers that run before an action hits the reducer.
 //
@@ -8,24 +11,22 @@ import { rootReducer } from "./root-reducer";
 
 // Instead of our own loggerMiddleware we can use the import logger line 2 instead!
 // is just a sequence of curried functions
-const loggerMiddleware = (store)  => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
 
 
-    console.log('next state: ', store.getState());
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],
 }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: [loggerMiddleware],
+    reducer: persistedReducer,
+    middleware: [process.env.NODE_ENV !== 'production' && loggerMiddleware].filter(Boolean),
 });
+
+export const persistor = persistStore(store);
 // Old way
 // const middleWares = [logger]
 //
