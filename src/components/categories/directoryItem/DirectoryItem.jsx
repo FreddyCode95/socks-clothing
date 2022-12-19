9984c278087401a339e64380c8376097ff1ed709
Nielsen <fredenielsen@deloitte.dk>
2022-12-19 15:31:07 +0100

    home routes now works though DirectoryItem category.route

--- src/components/categories/directoryItem/DirectoryItem.jsx
+++ src/components/categories/directoryItem/DirectoryItem.jsx
@@ -1,23 +1,24 @@
 import './DirectoryItem.scss';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
     return (
+            <div className='directory-item-container' onClick={onNavigateHandler}>
+                <div
+                    className='background-image'
+                    style={{
+                        backgroundImage: `url(${imageUrl})`,
+                    }}
+                />
+                <div className='body'>
+                    <h2>{title}</h2>
+                    <p>Shop Now</p>
+                </div>
 
-        <div className='directory-item-container'>
-            <div
-                className='background-image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`,
-                }}
-            />
-            <div className='body'>
-                <h2>{title}</h2>
-                <p>Shop Now</p>
             </div>
-
-        </div>
-
     );
 };
