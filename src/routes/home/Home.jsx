2abc6265cdea4d358aa8ddc6bf2f4fe2109a0e35
Nielsen <fredenielsen@deloitte.dk>
2022-12-21 14:51:43 +0100

    added popup for fun

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,38 +1,59 @@
 import {Categories} from "../../components/categories/Categories";
+import Swal from "sweetalert2";
+import {useEffect, useState} from "react";
+import {Alert} from "../../components/alert/Alert";
 
 export const Home = () => {
+
+    const rootPath = 'shop';
     const categories = [
         {
             id: 1,
             title: 'hats',
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            route: 'shop/hats',
+            route: `${rootPath}/hats`,
         },
         {
             id: 2,
             title: 'jackets',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            route: 'shop/jackets',
+            route: `${rootPath}/jackets`,
         },
         {
             id: 3,
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            route: 'shop/sneakers',
+            route: `${rootPath}/sneakers`,
         },
         {
             id: 4,
             title: 'womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-            route: 'shop/womens',
+            route: `${rootPath}/womens`,
         },
         {
             id: 5,
             title: 'mens',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-            route: 'shop/mens',
+            route: `${rootPath}/mens`,
         },
     ];
+
+
+
+    // const [countdown, setCountdown] = useState(10)
+    // useEffect(() => {
+    //     if (countdown > 0) {
+    //         setTimeout(()=> setCountdown(countdown - 1) ,1000)
+    //     } else {
+    //         setCountdown("boom")
+    //     }
+    // },[countdown])
+    //
+
+
+    // popup
+    Alert();
 
     return (
         <Categories categories={categories} />
